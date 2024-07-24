3f05e93c39e807eb884b43fed321034a4cde251b
Lucas Souza <lucaslongor1@gmail.com>
2024-07-24 14:26:45 -0300

    Concluido

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using DesafioFundamentos.Models;
+using System;
+using System.Collections.Generic;
 
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
